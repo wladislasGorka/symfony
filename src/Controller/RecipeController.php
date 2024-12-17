527b680d805e3d65080a7f6cabc49ca4f57537a0
wladislas.gorka <wladislas.gorka@gmail.com>
2024-12-17 16:23:09 +0100

    form

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,6 +2,10 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
+use App\Entity\Recipe;
+use App\Form\RecipeType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
@@ -10,16 +14,41 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'recipe.index')]
-    public function index(Request $request): Response
+    #[Route('/recipes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
     {
         //dd($request->attributes->get('slug'), $request->attributes->get('id'));
         //return new Response('Recipes');
-        return $this->render('recipe/index.html.twig');
+        $recipes = $repository->findWithDurationLowerThan(20);
+        //$recipes = $em->getRepository(Recipe::class)->findWithDurationLowerThan(20);
+
+        // Update recipe
+        // $recipes[0]->setTitle('Pâtes sauce tomate');
+        // $em->flush();
+
+        // Create recipe
+        // $recipe = new Recipe();
+        // $recipe->setTitle('Barbe à papa')
+        //     ->setSlug('barbe-papa')
+        //     ->setContent('Mettez du sucre.')
+        //     ->setCreatedAt(new \DateTimeImmutable())
+        //     ->setUpdateAt(new \DateTimeImmutable())
+        //     ->setDuration(5);
+        // $em->persist($recipe);
+        // $em->flush();
+
+        // Remove recipe
+        // $em->remove($recipes[0]);
+        // $em->flush();
+        // $recipes = $repository->findWithDurationLowerThan(20);
+
+        return $this->render('recipe/index.html.twig', [
+            'recipes'=> $recipes
+        ]);
     }
 
-    #[Route('/recipe/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    #[Route('/recipes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
         //dd($request->attributes->get('slug'), $request->attributes->get('id'));
         //dd($slug, $id);
@@ -30,13 +59,21 @@ class RecipeController extends AbstractController
         // return $this->json([
         //     'slug' => $slug
         // ]);
+        $recipe = $repository->find($id);
+        if($recipe->getSlug() != $slug) {
+            return $this->redirectToRoute('recipe.show', ['slug'=> $recipe->getSlug(), 'id'=> $recipe->getId()]);
+        }
         return $this->render('recipe/show.html.twig', [
-            'slug' => $slug,
-            'id'=> $id,
-            'person'=> [
-                'firstname' => 'John',
-                'lastname' => 'Doe'
-            ]
+            'recipe'=> $recipe
+        ]);
+    }
+
+    #[Route('/recipes/{id}/edit', name:'recipe.edit', requirements: ['id' => '\d+'])]
+    public function edit(Recipe $recipe, Request $request){
+        $form = $this->createForm(RecipeType::class, $recipe);
+        return $this->render('recipe/edit.html.twig', [
+            'recipe'=> $recipe,
+            'form'=> $form
         ]);
     }
 }
--- src/Repository/RecipeRepository.php
+++ src/Repository/RecipeRepository.php
@@ -16,6 +16,26 @@ class RecipeRepository extends ServiceEntityRepository
         parent::__construct($registry, Recipe::class);
     }
 
+
+    public function findTotalDuration(): int{
+        return $this->createQueryBuilder("r")
+            ->select('SUM(r.duration) as total')
+            ->getQuery()
+            ->getSingleScalarResult();        
+    }
+
+    /**
+     * @return Recipe[]
+     */
+    public function findWithDurationLowerThan(int $duration): array
+    {
+        return $this->createQueryBuilder("r")
+            ->where("r.duration <= :duration ")
+            ->orderBy("r.duration","ASC")
+            ->setParameter("duration", $duration)
+            ->getQuery()
+            ->getResult();
+    }
     //    /**
     //     * @return Recipe[] Returns an array of Recipe objects
     //     */

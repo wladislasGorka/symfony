82db5a95e24855f4f5b5e80c5f3fc578901661eb
wladislas.gorka <wladislas.gorka@gmail.com>
2024-12-17 12:31:10 +0100

    bdd migrate

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,6 +11,6 @@ class HomeController extends AbstractController
 {
     #[Route("/", name: "home")]
     function index(Request $request): Response {
-        return new Response("Bonjour " . $request->query->get("name","visiteur"));
+        return $this->render("home/index.html.twig");
     }
 }
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -3,15 +3,40 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class RecipeController extends AbstractController
 {
-    #[Route('/recipe/{slug}-{id}', name: 'recipe.show')]
+    #[Route('/recipe', name: 'recipe.index')]
     public function index(Request $request): Response
     {
-        dd($request);
+        //dd($request->attributes->get('slug'), $request->attributes->get('id'));
+        //return new Response('Recipes');
+        return $this->render('recipe/index.html.twig');
+    }
+
+    #[Route('/recipe/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id): Response
+    {
+        //dd($request->attributes->get('slug'), $request->attributes->get('id'));
+        //dd($slug, $id);
+        //return new Response('Recipe: ' . $slug);
+        // return new JsonResponse([
+        //     'slug' => $slug
+        // ]);
+        // return $this->json([
+        //     'slug' => $slug
+        // ]);
+        return $this->render('recipe/show.html.twig', [
+            'slug' => $slug,
+            'id'=> $id,
+            'person'=> [
+                'firstname' => 'John',
+                'lastname' => 'Doe'
+            ]
+        ]);
     }
 }

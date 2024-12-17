70ac0751cb5452a850dc55fac5f6196ed6b5f949
wladislas.gorka <wladislas.gorka@gmail.com>
2024-12-18 00:16:35 +0100

    constraints

--- src/Form/RecipeType.php
+++ src/Form/RecipeType.php
@@ -8,23 +8,35 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormEvents;
 use Symfony\Component\Form\FormEvent;
 use Symfony\Component\String\Slugger\AsciiSlugger;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Sequentially;
 
 class RecipeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('slug', TextType::class, [
-                'required' => false
+            ->add('title', TextType::class, [
+                'empty_data' => ''
             ])
-            ->add('content')
+            ->add('slug', TextType::class, [
+                'required' => false,
+                'constraints' => new Sequentially([
+                    new Length(min: 10), 
+                    new Regex('/^[a-z0-9]+(?:-[a-z0-9]+)*$/')
+                ])
+            ])
+            ->add('content', TextareaType::class, [
+                'empty_data' => ''
+            ])
             ->add('duration')
             ->add('save', SubmitType::class, [
-                'label'=> 'Edit'
+                'label'=> 'Send'
             ])
             ->addEventListener(FormEvents::PRE_SUBMIT, [$this, 'autoSlug'])
             ->addEventListener(FormEvents::POST_SUBMIT, [$this, 'attachTimestamps'])
